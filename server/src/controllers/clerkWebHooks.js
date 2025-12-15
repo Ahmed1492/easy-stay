7e5d998825a869ec4e4305353bc541b2f88b1481
Ahmed <rmmhnrnrjr@gmail.com>
2025-12-15 21:49:37 +0200

    Fix webhook not being triggered

--- server/src/controllers/clerkWebHooks.js
+++ server/src/controllers/clerkWebHooks.js
@@ -17,7 +17,7 @@ const clerkwebhooks = async (req, res) => {
     const { data, type } = req.body;
 
     const userData = {
-      clerkId: data.id,
+      _id: data.id,
       email: data.email_addresses[0].email_address,
       username: `${data.first_name} ${data.last_name}`,
       image: data.image_url,
