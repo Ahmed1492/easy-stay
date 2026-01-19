4fdfbbe01186da39cd4971957efb1fef3fcb115d
Ahmed <rmmhnrnrjr@gmail.com>
2026-01-19 14:35:15 +0200

    Add Stripe payment integration

--- server/src/controllers/stripeWebHook.js
+++ server/src/controllers/stripeWebHook.js
@@ -3,30 +3,31 @@ import Booking from "../../db/models/booking.model.js";
 
 // handle stripe webhooks
 export const stripeWebHooks = async (request, response) => {
+  // stripe gateway intialize
   const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
   const sig = request.headers['stripe-signature'];
-
   let event;
-
   try {
-    event = stripeInstance.webhooks.constructEvent(
-      request.body,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
+    event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPTE_WEBHOOK_SECRET);
   } catch (error) {
     return response.status(400).send(`Webhook Error: ${error.message}`);
   }
+  // handle the event
+  if (event.type === "payment_intent.succeeded") {
+    const paymentIntent = event.data.object;
+    const paymentIntentId = paymentIntent.id;
 
-  if (event.type === "checkout.session.completed") {
-    const session = event.data.object;
-    const { bookingId } = session.metadata;
-
-    await Booking.findByIdAndUpdate(bookingId, {
-      isPaid: true,
-      payment: "Stripe"
+    // getting session metaData
+    const session = await stripeInstance.checkout.sessions.list({
+      payment_intent: paymentIntentId
     });
-  }
+    const { bookingId } = session.data[0].metadata;
 
+    // mark payment as paid
+    await Booking.findByIdAndUpdate(bookingId, { isPaid: true, payment: "Stripe" });
+  } else {
+    console.log("Unhandled event type : ", event.type);
+
+  }
   response.json({ received: true });
 };
